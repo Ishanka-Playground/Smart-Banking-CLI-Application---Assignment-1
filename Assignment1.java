862a5246446dd0df727b39c27c8f3f0d4027acd4
Ishanka Dilshan <ishankadilshan64@gmail.com>
2023-08-15 21:46:18 +0530

    commit 1

---
+++ Assignment1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Assignment1 {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        final String CLEAR = "\033[H\033[2J";
+        final String COLOR_BLUE_BOLD = "\033[34;1m";
+        final String COLOR_RED_BOLD = "\033[31;1m";
+        final String COLOR_GREEN_BOLD = "\033[33;1m";
+        final String RESET = "\033[0m";
+
+        final String DASHBOARD = "\u1F4B0 Welcome to Smart Banking App";
+        final String ADD_ACCOUNT = "Open new Account";
+        final String DEPOSIT_MONEY = "Deposit Money";
+        final String WITHDRAW_MONEY = "Withdraw Money";
+        final String TRANSFER_MONEY = "Transfer Money";
+        final String CHECK_AC_BALANCE = "Check Account Balance";
+        final String DROP_EXISTING_AC = "Drop Existing Account";
+
+        final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
+        final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
+
+        String screen = DASHBOARD;
+
+
+
+
+
+        
+    }
+}
